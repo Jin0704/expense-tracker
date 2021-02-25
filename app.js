c4af123e1bd6c191ac2817049195ce39b36ad664
Jin <s87160204@gmail.com>
2021-02-25 23:22:23 +0800

    Feat: add message partial

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const usePassport = require('./config/passport')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const Record = require('./models/Record')
 const hdb = require('handlebars')
@@ -31,10 +32,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
