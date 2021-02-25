5a62cc5ae2d6cc3f2eccd11808a4c20248a3a008
Jin <s87160204@gmail.com>
2021-02-25 17:30:26 +0800

    feat:Add mongoose and Record model

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+
+const mongoose = require('mongoose')  // 載入 mongoose
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
 const app = express()
 
 
---
+++ models/Record.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const RecordSchema = new Schema({
+  name: String,
+  category: String,
+  date: String,
+  amount: Number,
+  totalAmount: String,
+  categoryName: String,
+  icon: String,
+  merchant: String,
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Record', RecordSchema)
