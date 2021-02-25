d417a704593009a5c6546dcf9fb0f51a80df9dc7
Jin <s87160204@gmail.com>
2021-02-25 18:20:43 +0800

    Feat:modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 const mongoose = require('mongoose')  // 載入 mongoose
+const Record = require('./models/Record')
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 const db = mongoose.connection
 
@@ -20,7 +21,10 @@ app.set('view engine', 'hbs')
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(error => console.error(error))
 })
 
 
